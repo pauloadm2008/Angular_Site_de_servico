1e86204dbb3b948cc3a21e3f1224caf839127e44
Paulo D'Almeida <paulo.dalmeida@trsolucoes.com>
2023-06-05 16:16:50 -0300

    feat: pagina de cadastro com angular e bootstrap.@2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CadastroComponent,
+    HomeComponent,
+    
+      ],
   imports: [
     BrowserModule
   ],
